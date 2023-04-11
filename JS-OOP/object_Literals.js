1f41884fc5b03b7b88e83aecf0ac597c071a7242
kipmyk <kipmyk@gmail.com>
2023-04-11 12:43:06 +0300

    JavaScript Object Literal

---
+++ JS-OOP/object_Literals.js
@@ -0,0 +1,15 @@
+// define const
+/**
+ * @link ref: https://playcode.io/javascript/object-literal
+ * */ 
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw:  function(){
+        console.log('draw');
+    }
+};
+circle.draw();
