7e24b67e017532f4c18f2d7f1d6c162d11bf8e96
kipmyk <kipmyk@gmail.com>
2023-10-23 12:50:58 +0300

    factory function

--- JS-OOP/Factory Function/index.js
+++ JS-OOP/Factory Function/index.js
@@ -13,3 +13,30 @@ function createPerson(firstName, lastName) {
   
   console.log(person1.getFullName());
   console.log(person2.getFullName());
+
+      // Factory Function creating person 
+      var Person = function (name, age) { 
+  
+        // creating person object 
+        var person = {}; 
+  
+        // parameters as keys to this object   
+        person.name = name; 
+        person.age = age; 
+  
+        // function to greet 
+        person.greeting = function () { 
+            return ( 
+                'Hello I am ' + person.name  
+                    + '. I am ' + person.age  
+                    + ' years old. '
+            ); 
+        }; 
+        return person; 
+    }; 
+  
+    var person3 = Person('Abhishek', 20); 
+    console.log(person1.greeting()); 
+  
+    var person4 = Person('Raj', 25); 
+    console.log(person2.greeting());
